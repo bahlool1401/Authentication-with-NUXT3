2d388e61901ce17fe273bbd321fc0058ef5787fb
bahlool1401 <bahlool.ramesh@gmail.com>
2025-01-16 23:49:17 +0330

    add "next-auth" package and edit in app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { defineAppConfig } from 'nuxt/app'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -10,5 +13,23 @@ export default defineNuxtConfig({
   },
   css: ['~/assets/css/main.css'],
 
+  modules:[
+    // "@nuxtjs/tailwindcss",
+    "nuxt-server-utils",
+    "@sidebase/nuxt-auth"
+  ],
 
+  nuxtServerUtils: {
+    mongodbUri: process.env.MONGODB_URI,
+  },
+  
+
+
+  //* this is for Authentication package👇👇
+  auth:{
+    provider:{
+      type:'authjs'
+    }
+  }
+  // authjs is a JS package which provides a different authentication services
 })
